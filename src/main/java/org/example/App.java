5c642a5f7604db5888181fa433b3d68afc66be45
Artem Hubar <hubar.artem@gmail.com>
2024-01-17 22:26:00 +0200

    initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        СalcSM calc = new СalcSM();
+        Scanner sc = new Scanner(System.in);
+        Integer cmd;
+        while ( (cmd = sc.nextInt()) != 0){
+            if(cmd==1){
+                calc.action(СalcSM.CMD.INPUT);
+            } else if (cmd == 2) {
+                calc.action(СalcSM.CMD.CANCEL);
+            }else if(cmd == 3){
+                calc.action((СalcSM.CMD.BEGIN));
+            }
+        }
     }
 }
